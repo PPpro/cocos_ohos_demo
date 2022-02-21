d3d51cbc19b82e6fd1ca9cdef013a3672d30b6f0
PP_Pro <chinese1236@qq.com>
2022-02-21 18:13:29 +0800

    import map

--- entry/src/main/ets/default/cocos/game.ts
+++ entry/src/main/ets/default/cocos/game.ts
@@ -46,7 +46,9 @@ const commonJSModuleMap: Record<string, Function> = {
     '/src/application.79b93.js' () { require('./src/application.79b93.js'); },
     '/src/cocos-js/cc.js' () { require('./src/cocos-js/cc.js'); },
     '/src/src/chunks/bundle.js' () { require('./src/chunks/bundle.js') },
-    'workers/assets/main/index.js' () { require('./assets/main/index.js'); }
+    'workers/assets/main/index.js' () { require('./assets/main/index.js'); },
+    '/src/cocos-js/wait-for-ammo-instantiation.js' () { require('./src/cocos-js/wait-for-ammo-instantiation.js'); },
+    '/src/cocos-js/instantiated-1af0bf5b.js' () { require('./src/cocos-js/instantiated-1af0bf5b.js'); }
 }
 export function loadModule (name: string) {
     const moduleExecutor = commonJSModuleMap[name];
