80765ab24655d92bb1954f2ba24a6fed3fefe93a
PP_Pro <chinese1236@qq.com>
2022-02-22 11:24:01 +0800

    fix bundle

--- entry/src/main/ets/default/cocos/game.ts
+++ entry/src/main/ets/default/cocos/game.ts
@@ -45,7 +45,7 @@ console.timeEnd = function () {}
 const commonJSModuleMap: Record<string, Function> = {
     '/src/application.79b93.js' () { require('./src/application.79b93.js'); },
     '/src/cocos-js/cc.js' () { require('./src/cocos-js/cc.js'); },
-    '/src/src/chunks/bundle.js' () { require('./src/chunks/bundle.js') },
+    '/src/chunks/bundle.js' () { require('./src/chunks/bundle.js') },
     'workers/assets/main/index.js' () { require('./assets/main/index.js'); },
     '/src/cocos-js/wait-for-ammo-instantiation.js' () { require('./src/cocos-js/wait-for-ammo-instantiation.js'); },
     '/src/cocos-js/instantiated-1af0bf5b.js' () { require('./src/cocos-js/instantiated-1af0bf5b.js'); }
