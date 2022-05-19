6d2a6b029e8357f3531efad363cd9f2fc05065cb
PP_Pro <chinese1236@qq.com>
2022-05-19 19:35:02 +0800

    支持命令行构建

---
+++ install_run.bat
@@ -0,0 +1,10 @@
+set tool=C:\Users\l\AppData\Local\OpenHarmony\Sdk\toolchains\3.1.5.5\hdc_std.exe
+set pack_local_path=C:\Users\l\Desktop\cocos_ohos_demo\entry\build\default\outputs\default\entry-default-signed.hap
+set pack_name=ohos.example.test
+
+%tool% uninstall %pack_name%
+%tool% install %pack_local_path%
+
+%tool% shell aa start -b %pack_name% -a %pack_name%.MainAbility
+
+%tool% hilog -r > ./log.txt
