f39b5e2c67a627f51186a3b86565f1dde00d80a3
PP_Pro <chinese1236@qq.com>
2022-06-22 17:40:01 +0800

    update

--- install_run.bat
+++ install_run.bat
@@ -1,5 +1,5 @@
 set tool=C:\Users\l\AppData\Local\OpenHarmony\Sdk\toolchains\3.1.5.5\hdc_std.exe
-set pack_local_path=C:\Users\l\Desktop\cocos_ohos_demo\entry\build\default\outputs\default\entry-default-signed.hap
+set pack_local_path=C:\Users\l\Desktop\open_harmony\cocos_ohos_demo\entry\build\default\outputs\default\entry-default-signed.hap
 set pack_name=ohos.example.test
 
 %tool% uninstall %pack_name%
@@ -7,4 +7,4 @@ set pack_name=ohos.example.test
 
 %tool% shell aa start -b %pack_name% -a %pack_name%.MainAbility
 
-%tool% hilog -r > ./log.txt
+%tool% hilog -b D -D 0x0001 > ./log.txt
